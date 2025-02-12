2a0e5898f587e5d04ce90e2f8696df495500724e
Duong Tuan Kiet <73464915+anhdadenpp@users.noreply.github.com>
2025-02-12 14:50:33 +0700

    Section 16 : JS DOM

---
+++ Section_16_JS_DOM/DOM Challenge Starting Files/index.js
@@ -0,0 +1,8 @@
+//DOM is Document Object Model
+
+document.querySelector("h1").innerHTML = "Good Bye";
+document.querySelector("h1").innerText = "I am a new heading";
+document.querySelector("h1").click();
+document.getElementsByTagName("li")[0].style.color = "green";
+document.getElementsByTagName("li").length;
+document.getElementsByClassName("btn")[0].style.backgroundColor = "yellow";
